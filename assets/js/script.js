b20126640a9f13c7286231ebde2c9e5e5e4db0e3
Erik <erikhgm@gmail.com>
2022-05-28 05:59:59 +0000

    Tidy up the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // runs the eventlister thas is starting the showStartScreen once the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
     showStartScreen();
-})
+});
 // shows the startscreen upon entering the game and runs the startbutton eventlistener
 function showStartScreen() {
     document.getElementById('start-screen').style.display = "block";
@@ -10,7 +10,7 @@ function showStartScreen() {
 
 }
 // collets the name from the player
-let name = document.getElementById('name').innerText;
+let username = document.getElementById('name').innerText;
 
 // switches to the game screen and runs the cardsContainer function
 function showGameScreen() {
@@ -22,18 +22,14 @@ function showGameScreen() {
 // creates the HTML structure for the cardHolders in the gamescreen
 function cardsContainers() {
     for (let i = 0; i < 16; i++) {
-        let cardHolder = document.createElement('div')
-        cardHolder.className = 'cardHolder';
-        cardHolder.innerText = '?'
+        let cardHolder = document.createElement('div');
+        cardHolder.className = 'memory-card';
+        cardHolder.innerHTML = '<i class="fa-solid fa-question"></i>';
         cardHolder.addEventListener('click', showCard);
         document.getElementById('game').appendChild(cardHolder);
 
     }
-    for (let i = 0; i < 16; i++) {
-        let i = document.createElement('div')
-        i.className = 'i';
-        document.getElementById('game').appendChild(i);
-    }
+   
     insertCardContent();
 
 }
@@ -61,21 +57,21 @@ function insertCardContent() {
 
     // creates an ordered array with numbers less than 16
     let number = [];
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < cardDeck.length; i++) {
         number.push(i);
     }
     // Sort the above number array into a random order each time it is executed.
     // Credit: this code snippet comes from https://www.w3schools.com/js/js_array_sort.asp.
     for (let i = number.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * i)
-        let k = number[i]
-        number[i] = number[j]
-        number[j] = k
+        let j = Math.floor(Math.random() * i);
+        let k = number[i];
+        number[i] = number[j];
+        number[j] = k;
     }
     // inserts an item into the game screen cardHolder
-    for (let i = 0; i < 16; i++) {
-        let card = document.getElementsByClassName('i');
-        card[i].innerText = cardDeck[number[i]];
+    for (let i = 0; i < cardDeck.length; i++) {
+        let card = document.getElementsByClassName('icon');
+        card[i].innerHTML = cardDeck[number[i]];
     }
 
     showCard();
@@ -83,13 +79,8 @@ function insertCardContent() {
 
 
 function showCard() {
-    console.log('clicked')
-    console.log(this)
-    console.log(this.classList)
-    if (this.classList.contains('cardHolder')) {
-        console.log("condition met")
-        this.setAttribute('class','i');
-        this.style.display = 'inline-block';
+    if (this.classList.contains('memory-card')) {
+        this.style.background-color; 'red;';
     }
 
 }
